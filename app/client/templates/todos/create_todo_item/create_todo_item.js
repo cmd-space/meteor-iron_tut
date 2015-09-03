cde9b0657b80abfaffd7862f768df0695135870d
Mason Crane <masonecrane@gmail.com>
2015-09-02 21:49:09 -0600

    Created four templates

---
+++ app/client/templates/todos/create_todo_item/create_todo_item.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* CreateTodoItem: Event Handlers */
+/*****************************************************************************/
+Template.CreateTodoItem.events({
+});
+
+/*****************************************************************************/
+/* CreateTodoItem: Helpers */
+/*****************************************************************************/
+Template.CreateTodoItem.helpers({
+});
+
+/*****************************************************************************/
+/* CreateTodoItem: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreateTodoItem.onCreated(function () {
+});
+
+Template.CreateTodoItem.onRendered(function () {
+});
+
+Template.CreateTodoItem.onDestroyed(function () {
+});
